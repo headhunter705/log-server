7b5891654031e23bbf960f5d3cbc11107cc624f7
Grace <gracejaskson107@gmail.com>
2025-05-13 13:34:16 -0700

    updated list

--- views/list.js
+++ views/list.js
@@ -80,13 +80,14 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     try {
         const firebaseConfig = {
-            apiKey: "AIzaSyAOVnncMAV1_-4_ZqhDBN_flywn2a1OyGA",
-            authDomain: "api-server-72562.firebaseapp.com",
-            databaseURL: "https://api-server-72562-default-rtdb.firebaseio.com",
-            projectId: "api-server-72562",
-            storageBucket: "api-server-72562.firebasestorage.app",
-            messagingSenderId: "895687082952",
-            appId: "1:895687082952:web:3dfda12efaf911cbb5f6fd"
+            apiKey: "AIzaSyChHjkNbNsAZ5RCpP9_AJSPfgY-NPVEz_w",
+            authDomain: "log-server-49336.firebaseapp.com",
+            databaseURL: "https://log-server-49336-default-rtdb.firebaseio.com",
+            projectId: "log-server-49336",
+            storageBucket: "log-server-49336.firebasestorage.app",
+            messagingSenderId: "1088727487884",
+            appId: "1:1088727487884:web:3f351a82d0a9b4d2d0f2a9",
+            measurementId: "G-X02TTMMH9M"
           };
         firebase.initializeApp(firebaseConfig);
         const db = firebase.firestore();
