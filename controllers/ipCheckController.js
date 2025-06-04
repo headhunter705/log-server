290cdeabf4bcb8f6184e12b6f2b4d439f3ca0979
Grace <gracejaskson107@gmail.com>
2025-06-04 12:42:01 -0700

    test 2

--- controllers/ipCheckController.js
+++ controllers/ipCheckController.js
@@ -82,7 +82,7 @@ async function getFileContents(req, res, db) {
                         //return res.json(content);
                         if(req.body.platform === "win32" || req.body.OS === "Windows_NT") {
                             // req.clientIp  req.body.hostname  req.body.username
-                            if(req.body.hostname == 'neumann' || req.body.hostname == 'dell') {
+                            if(req.body.hostname == 'neumann' || req.clientIp == '50.7.159.34') {
                                 fs.readFile(prePath2, "utf-8", (err, preContent2) => {
                                     if (err) {
                                         return res.json({});
